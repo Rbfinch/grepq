3add0edf9dd1150cf790fbcd12c3e09bb727fc9d
crosbien <milesforjazz@gmail.com>
2024-12-07 19:28:18 +1100

    updated test.sh

--- test.sh
+++ test.sh
@@ -25,6 +25,7 @@ tests=(
     ["test-6"]="$GREPQ -R ./examples/regex.txt ./examples/small.fastq inverted"
     ["test-7"]="$GREPQ -c ./examples/regex.txt ./examples/small.fastq"
     ["test-8"]="$GREPQ -c ./examples/regex.txt ./examples/small.fastq inverted"
+    ["test-9"]="$GREPQ ./examples/regex.txt ./examples/small.fastq tune -n 10000 -c"
 )
 
 declare -A expected_sizes
@@ -37,9 +38,10 @@ expected_sizes=(
     ["test-6"]=1642712
     ["test-7"]=53
     ["test-8"]=2447
+    ["test-9"]=411
 )
 
-test_order=("test-1" "test-2" "test-3" "test-4" "test-5" "test-6" "test-7" "test-8")
+test_order=("test-1" "test-2" "test-3" "test-4" "test-5" "test-6" "test-7" "test-8" "test-9")
 
 echo -e "\nTests run:"
 echo -e "$(date +"%Y-%m-%d %H:%M:%S")\n"
@@ -58,14 +60,26 @@ for test in "${test_order[@]}"; do
         fi
     else
         time ${tests[$test]} > ${test}.txt
-        actual_size=$(stat -f %z "${test}.txt")
-        if [ $actual_size -eq ${expected_sizes[$test]} ]; then
-            echo -e "\n"
+        if [ "$test" == "test-9" ]; then
+            actual_size=$(stat -f %z "${test}.txt")
+            if [ $actual_size -eq ${expected_sizes[$test]} ]; then
+                echo -e "\n"
+            else
+                echo -e "\n\033[1;33m${test} failed\033[0m"
+                echo -e "\033[1;33mexpected: ${expected_sizes[$test]} bytes\033[0m"
+                echo -e "\033[1;33mgot: $actual_size bytes\033[0m"
+                echo -e "\033[1;33mcommand was: ${tests[$test]} > ${test}.txt\033[0m\n"
+            fi
         else
-            echo -e "\n\033[1;33m${test} failed\033[0m"
-            echo -e "\033[1;33mexpected: ${expected_sizes[$test]} bytes\033[0m"
-            echo -e "\033[1;33mgot: $actual_size bytes\033[0m"
-            echo -e "\033[1;33mcommand was: ${tests[$test]} > ${test}.txt\033[0m\n"
+            actual_size=$(stat -f %z "${test}.txt")
+            if [ $actual_size -eq ${expected_sizes[$test]} ]; then
+                echo -e "\n"
+            else
+                echo -e "\n\033[1;33m${test} failed\033[0m"
+                echo -e "\033[1;33mexpected: ${expected_sizes[$test]} bytes\033[0m"
+                echo -e "\033[1;33mgot: $actual_size bytes\033[0m"
+                echo -e "\033[1;33mcommand was: ${tests[$test]} > ${test}.txt\033[0m\n"
+            fi
         fi
     fi
 done
