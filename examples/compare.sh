7687657755ec664e3c8aee295b833dd2ab081a73
crosbien <milesforjazz@gmail.com>
2024-12-15 07:56:39 +1100

    updated

--- examples/compare.sh
+++ examples/compare.sh
@@ -3,20 +3,44 @@
 ### Compare two snapshot times 
 # Author: Nicholas D. Crosbie
 # Date: December 2024 
+# Usage: $0 [-t tolerance] <path_to_first_input_file> <path_to_second_input_file>
 ###
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Default tolerance difference (in percentage)
+TOLERANCE=10
+
+# Parse command-line options
+while getopts ":t:" opt; do
+  case $opt in
+    t)
+      TOLERANCE=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Shift the parsed options away
+shift $((OPTIND -1))
+
 # Check if the path to the input file is provided
 if [ -z "$1" ]; then
-    echo "Usage: $0 <path_to_first_input_file>"
+    echo "Usage: $0 [-t tolerance] <path_to_first_input_file> <path_to_second_input_file>"
     exit 1
 fi
 
 # Check if the path to the second input file is provided
 if [ -z "$2" ]; then
-    echo "Usage: $0 <path_to_first_input_file> <path_to_second_input_file>"
+    echo "Usage: $0 [-t tolerance] <path_to_first_input_file> <path_to_second_input_file>"
     exit 1
 fi
 
@@ -27,6 +51,11 @@ echo -e
 echo -e "Comparing times in control: $INPUT_FILE"
 echo -e "to test: $SECOND_INPUT_FILE"
 echo -e
+
+# Color codes
+BOLD="\033[1m"
+ORANGE="\033[38;2;255;165;0m"
+RESET="\033[0m"
 
 # Function to process input file and extract times in seconds
 extract_times() {
@@ -59,5 +88,9 @@ for i in "${!times_file1[@]}"; do
     time1=${times_file1[$i]}
     time2=${times_file2[$i]}
     percent_difference=$(echo "scale=2; (($time2 - $time1) / $time1) * 100" | bc)
-    echo "%diff test-$((i + 1)): $percent_difference%"
+    if (( $(echo "$percent_difference > $TOLERANCE" | bc -l) )) || (( $(echo "$percent_difference < -$TOLERANCE" | bc -l) )); then
+        echo -e "${BOLD}${ORANGE}%diff test-$((i + 1)): $percent_difference%${RESET}"
+    else
+        echo "%diff test-$((i + 1)): $percent_difference%"
+    fi
 done
--- examples/test.sh
+++ examples/test.sh
@@ -9,8 +9,8 @@
 # add Linux support for stat command
 
 ## Redirect all output to a file, with the current date and time as the filename
-# ./test.sh tests.yaml &> ../snapshots/$(date +"%Y-%m-%d %H:%M:%S").txt
-# ./test.sh control tests.yaml &> ../snapshots/$(date +"%Y-%m-%d %H:%M:%S").txt
+# ./test.sh tests.yaml &> ../snapshots/$(date +"%Y-%m-%d-%H:%M:%S").txt
+# ./test.sh control tests.yaml &> ../snapshots/$(date +"%Y-%m-%d-%H:%M:%S").txt
 
 # Exit immediately if a command exits with a non-zero status
 set -e
