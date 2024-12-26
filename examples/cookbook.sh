6083c77365dfd4fb08257327d2d9d9297269d556
Nicholas Crosbie <nicholascrosbie@Mac.lan>
2024-12-26 12:23:21 +1100

    updated

--- examples/cookbook.sh
+++ examples/cookbook.sh
@@ -17,73 +17,82 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo -e "\nPrint only the matching sequences"
-echo -e "\nCommand: grepq 16S-no-iupac.txt small.fastq | head -n 10 \n"
-grepq 16S-no-iupac.txt small.fastq | head -n 10
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
-echo -e "\nPrint the matching sequences with the record ID"
-echo -e "\nCommand: grepq -I 16S-no-iupac.txt small.fastq | head -n 10 \n"
-grepq -I 16S-no-iupac.txt small.fastq | head -n 10
+echo -e "${BLUE}\nSave only the matching sequences${NC}"
+echo -e "${BLUE}\nCommand: grepq 16S-no-iupac.txt small.fastq | head -n 10 \n${NC}"
+grepq 16S-no-iupac.txt small.fastq > out.txt
+head -n 10 out.txt
 
-echo -e "\nPrint the matching sequences in FASTQ format"
-echo -e "\nCommand: grepq -R 16S-no-iupac.txt small.fastq | head -n 10 \n"
-grepq -R 16S-no-iupac.txt small.fastq | head -n 10
+echo -e "${BLUE}\nSave the matching sequences with the record ID${NC}"
+echo -e "${BLUE}\nCommand: grepq -I 16S-no-iupac.txt small.fastq | head -n 10 \n${NC}"
+grepq -I 16S-no-iupac.txt small.fastq > out.txt
+head -n 10 out.txt
 
-echo -e "\nSave the matching sequences in gzip compressed FASTQ format"
-echo -e "\nCommand: grepq -R -z 16S-no-iupac.txt small.fastq > output.fastq.gz \n"
-grepq -R -z 16S-no-iupac.txt small.fastq > output.fastq.gz
+echo -e "${BLUE}\nSave the matching sequences in FASTQ format${NC}"
+echo -e "${BLUE}\nCommand: grepq -R 16S-no-iupac.txt small.fastq | head -n 10 \n${NC}"
+grepq -R 16S-no-iupac.txt small.fastq > out.txt
+head -n 10 out.txt
 
-echo -e "\nRead the FASTQ file in gzip compressed format and save only the
-matching sequences"
-echo -e "\nCommand: grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz > output.txt \n"
-grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz > output.txt
+echo -e "${BLUE}\nSave the matching sequences in gzip compressed FASTQ format${NC}"
+echo -e "${BLUE}\nCommand: grepq -R -z 16S-no-iupac.txt small.fastq > out.fastq.gz \n${NC}"
+grepq -R -z 16S-no-iupac.txt small.fastq > out.fastq.gz
+gunzip -q out.fastq.gz
+head -n 10 out.fastq
 
-echo -e "\nRead and save the output in gzip compressed format, with fast compression"
-echo -e "\nCommand: grepq -xz --fast 16S-no-iupac.txt SRX26365298.fastq.gz > output.fastq.gz \n"
-grepq -xz --fast 16S-no-iupac.txt SRX26365298.fastq.gz > output.fastq.gz
+echo -e "${BLUE}\nRead the FASTQ file in gzip compressed format and save only the matching sequences${NC}"
+echo -e "${BLUE}\nCommand: grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz > out.txt \n${NC}"
+grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz > out.txt
+head -n 10 out.txt
 
-echo -e "\nRead and save the output in gzip compressed format, with best compression"
-echo -e "\nCommand: grepq -xz --best 16S-no-iupac.txt SRX26365298.fastq.gz > output.fastq.gz \n"
-grepq -xz --best 16S-no-iupac.txt SRX26365298.fastq.gz > output.fastq.gz
+echo -e "${BLUE}\nRead and save the out in gzip compressed format, with fast compression${NC}"
+echo -e "${BLUE}\nCommand: grepq -xz --fast 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz \n${NC}"
+grepq -xz --fast 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz
+gunzip -q out.fastq.gz
+head -n 10 out.fastq
 
-echo -e "\nCount the number of matching FASTQ records"
-echo -e "\nCommand: grepq -c 16S-no-iupac.txt small.fastq \n"
+echo -e "${BLUE}\nRead and save the out in gzip compressed format, with best compression${NC}"
+echo -e "${BLUE}\nCommand: grepq -xz --best 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz \n${NC}"
+grepq -xz --best 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz
+gunzip -q out.fastq.gz
+head -n 10 out.fastq
+
+echo -e "${BLUE}\nCount the number of matching FASTQ records${NC}"
+echo -e "${BLUE}\nCommand: grepq -c 16S-no-iupac.txt small.fastq \n${NC}"
 grepq -c 16S-no-iupac.txt small.fastq
 
-echo -e "\nFor each matched pattern in a search of the first 100000 records,
-print the pattern and the number of matches"
-echo -e "\nCommand: grepq 16S-no-iupac.txt small.fastq tune -n 100000 -c \n"
+echo -e "${BLUE}\nFor each matched pattern in a search of the first 100000 records, print the pattern and the number of matches${NC}"
+echo -e "${BLUE}\nCommand: grepq 16S-no-iupac.txt small.fastq tune -n 100000 -c \n${NC}"
 grepq 16S-no-iupac.txt small.fastq tune -n 100000 -c
 
-echo -e "\nFor each matched pattern in a search of the first 100000 records of
-a gzip-compressed FASTQ file, print the pattern and the number of matches"
-echo -e "\nCommand: grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c \n"
+echo -e "${BLUE}\nFor each matched pattern in a search of the first 100000 records of a gzip-compressed FASTQ file, print the pattern and the number of matches${NC}"
+echo -e "${BLUE}\nCommand: grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c \n${NC}"
 grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c
 
-echo -e "\nFor each matched pattern in a search of the first 100000 records of
- a gzip-compressed FASTQ file, print the pattern and the number of matches to a
-JSON file called matches.json"
-echo -e "\nCommand: grepq -x 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches \n"
+echo -e "${BLUE}\nFor each matched pattern in a search of the first 100000 records of a gzip-compressed FASTQ file, print the pattern and the number of matches to a JSON file called matches.json${NC}"
+echo -e "${BLUE}\nCommand: grepq -x 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches \n${NC}"
 grepq -x 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches
 
-echo -e "\nSave the records where none of the regex patterns are found"
-echo -e "\nCommand: grepq 16S-no-iupac.txt small.fastq inverted > output.txt \n"
-grepq 16S-no-iupac.txt small.fastq inverted > output.txt
+echo -e "${BLUE}\nSave the records where none of the regex patterns are found${NC}"
+echo -e "${BLUE}\nCommand: grepq 16S-no-iupac.txt small.fastq inverted > out.txt \n${NC}"
+grepq 16S-no-iupac.txt small.fastq inverted > out.txt
+head -n 10 out.txt
 
-echo -e "\nSave the records where none of the regex patterns are found, with
-the record ID"
-echo -e "\nCommand: grepq -I 16S-no-iupac.txt small.fastq inverted > output.txt \n"
-grepq -I 16S-no-iupac.txt small.fastq inverted > output.txt
+echo -e "${BLUE}\nSave the records where none of the regex patterns are found, with the record ID${NC}"
+echo -e "${BLUE}\nCommand: grepq -I 16S-no-iupac.txt small.fastq inverted > out.txt \n${NC}"
+grepq -I 16S-no-iupac.txt small.fastq inverted > out.txt
+head -n 10 out.txt
 
-echo -e "\nPrint the records where none of the regex patterns are found, in FASTQ format"
-echo -e "\nCommand: grepq -R 16S-no-iupac.txt small.fastq inverted > output.txt \n"
-grepq -R 16S-no-iupac.txt small.fastq inverted > output.txt
+echo -e "${BLUE}\nSave the records where none of the regex patterns are found, in FASTQ format${NC}"
+echo -e "${BLUE}\nCommand: grepq -R 16S-no-iupac.txt small.fastq inverted > out.txt \n${NC}"
+grepq -R 16S-no-iupac.txt small.fastq inverted > out.txt
+head -n 10 out.txt
 
-echo -e "\nCount the number of records where none of the regex patterns are found"
-echo -e "\nCommand: grepq -c 16S-no-iupac.txt small.fastq inverted \n"
+echo -e "${BLUE}\nCount the number of records where none of the regex patterns are found${NC}"
+echo -e "${BLUE}\nCommand: grepq -c 16S-no-iupac.txt small.fastq inverted \n${NC}"
 grepq -c 16S-no-iupac.txt small.fastq inverted
 
-echo -e "\nCount the total number of records in the FASTQ file using an empty 
- pattern file"
-echo -e "\nCommand: grepq -c empty.txt file.fastq inverted \n"
+echo -e "${BLUE}\nCount the total number of records in the FASTQ file using an empty pattern file${NC}"
+echo -e "${BLUE}\nCommand: grepq -c empty.txt file.fastq inverted \n${NC}"
 grepq -c empty.txt small.fastq inverted
