106d7d9ef01e359d15fed55dc29af3f2e8fcd4bb
Nicholas Crosbie <nicholascrosbie@Mac.lan>
2025-01-12 13:21:44 +1100

    updates

--- examples/cookbook.sh
+++ examples/cookbook.sh
@@ -12,7 +12,7 @@
 # To run all examples, make sure to download the file SRX26365298.fastq.gz from
 # the SRA and place it in the `examples` directory. You can download the file 
 # with `fastq-dump --accession SRX26365298`. Obtain `fastq-dump` from the 
-# SRA Toolkit, available at NCBI.  =
+# SRA Toolkit, available at NCBI. 
 
 # Exit immediately if a command exits with a non-zero status
 set -e
@@ -36,25 +36,25 @@ grepq -R 16S-no-iupac.txt small.fastq > out.txt
 head -n 10 out.txt
 
 echo -e "${BLUE}\nSave the matching sequences in gzip compressed FASTQ format${NC}"
-echo -e "${BLUE}\nCommand: grepq -R -z 16S-no-iupac.txt small.fastq > out.fastq.gz \n${NC}"
-grepq -R -z 16S-no-iupac.txt small.fastq > out.fastq.gz
+echo -e "${BLUE}\nCommand: grepq -R --write-gzip 16S-no-iupac.txt small.fastq > out.fastq.gz \n${NC}"
+grepq -R --write-gzip 16S-no-iupac.txt small.fastq > out.fastq.gz
 gunzip -q out.fastq.gz
 head -n 10 out.fastq
 
 echo -e "${BLUE}\nRead the FASTQ file in gzip compressed format and save only the matching sequences${NC}"
-echo -e "${BLUE}\nCommand: grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz > out.txt \n${NC}"
-grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz > out.txt
+echo -e "${BLUE}\nCommand: grepq --read-gzip 16S-no-iupac.txt SRX26365298.fastq.gz > out.txt \n${NC}"
+grepq --read-gzip 16S-no-iupac.txt SRX26365298.fastq.gz > out.txt
 head -n 10 out.txt
 
 echo -e "${BLUE}\nRead and save the out in gzip compressed format, with fast compression${NC}"
-echo -e "${BLUE}\nCommand: grepq -xz --fast 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz \n${NC}"
-grepq -xz --fast 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz
+echo -e "${BLUE}\nCommand: grepq --read-gzip --write-gzip --fast 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz \n${NC}"
+grepq --read-gzip --write-gzip --fast 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz
 gunzip -q out.fastq.gz
 head -n 10 out.fastq
 
 echo -e "${BLUE}\nRead and save the out in gzip compressed format, with best compression${NC}"
-echo -e "${BLUE}\nCommand: grepq -xz --best 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz \n${NC}"
-grepq -xz --best 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz
+echo -e "${BLUE}\nCommand: grepq --read-gzip --write-gzip --best 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz \n${NC}"
+grepq --read-gzip --write-gzip --best 16S-no-iupac.txt SRX26365298.fastq.gz > out.fastq.gz
 gunzip -q out.fastq.gz
 head -n 10 out.fastq
 
@@ -67,12 +67,12 @@ echo -e "${BLUE}\nCommand: grepq 16S-no-iupac.txt small.fastq tune -n 100000 -c 
 grepq 16S-no-iupac.txt small.fastq tune -n 100000 -c
 
 echo -e "${BLUE}\nFor each matched pattern in a search of the first 100000 records of a gzip-compressed FASTQ file, print the pattern and the number of matches${NC}"
-echo -e "${BLUE}\nCommand: grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c \n${NC}"
-grepq -x 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c
+echo -e "${BLUE}\nCommand: grepq --read-gzip 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c \n${NC}"
+grepq --read-gzip 16S-no-iupac.txt SRX26365298.fastq.gz tune -n 100000 -c
 
 echo -e "${BLUE}\nFor each matched pattern in a search of the first 100000 records of a gzip-compressed FASTQ file, print the pattern and the number of matches to a JSON file called matches.json${NC}"
-echo -e "${BLUE}\nCommand: grepq -x 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches \n${NC}"
-grepq -x 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches
+echo -e "${BLUE}\nCommand: grepq --read-gzip 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches \n${NC}"
+grepq --read-gzip 16S-no-iupac.json SRX26365298.fastq.gz tune -n 100000 -c --names --json-matches
 
 echo -e "${BLUE}\nSave the records where none of the regex patterns are found${NC}"
 echo -e "${BLUE}\nCommand: grepq 16S-no-iupac.txt small.fastq inverted > out.txt \n${NC}"
